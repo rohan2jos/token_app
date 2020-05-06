1fe8ae52887071d8140a715536335831c9655686
rohan <rohan2jos@gmail.com>
2020-05-06 00:48:21 -0500

    freezing requirements; adding pyopenssl

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ if __name__ == "__main__":
     # if running on localhost, use the localhost
     # port
     port = int(os.environ.get("PORT", 5002))
-    APP.run(host='0.0.0.0', port=port)
+    APP.run(host='0.0.0.0', port=port, ssl_context='adhoc')
