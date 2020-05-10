8033a6de6ac8b9cd86c1804f41e0ccd5dfff2dec
rohan <rohan2jos@gmail.com>
2020-05-10 12:30:50 -0500

    fixing setup step to insert timeslots

--- utils/timeslot_engine.py
+++ utils/timeslot_engine.py
@@ -1,6 +1,7 @@
 import datetime
 import logging
 import sys
+import pytz
 
 from dba.timeslot_dba import TimeslotDBA
 
@@ -24,19 +25,27 @@ def setup_timeslot_db():
 
     # check if the timeslots db is present.  If not, we need to create a new one and set index
     does_db_exist = timeslot_dba.does_timeslot_db_exist()
+    date_today = generate_utc_date()
+
     if not does_db_exist:
         LOGGER.info('[SETUP] timeslot collection does not exist, creating')
         collection_create_response = timeslot_dba.create_collection()
+        LOGGER.info('[SETUP] Creating index on newly created db')
+        _ = timeslot_dba.create_timeslot_index('timeslot_index')
         if not collection_create_response:
             sys.exit()
-        _ = timeslot_dba.create_timeslot_index("timeslot_index")
-        LOGGER.info("[SETUP] created the index")
 
-    generated_timeslots = generate_time_slots_from_range('9:00', '18:00')
-    insert_response = timeslot_dba.insert_generated_timeslots(generated_timeslots)
-    if not insert_response:
-        LOGGER.error('There was a problem generating and inserting the timeslots')
-        sys.exit()
+    # check if there is a doc with todays date, if it exists, we don't need to create an index
+    LOGGER.info('[SETUP] Checking if there is a doc with todays date')
+    does_today_doc_exist = timeslot_dba.check_today_doc_exist(date_today)
+    if not does_today_doc_exist:
+        generated_timeslots = generate_time_slots_from_range('9:00', '18:00')
+        insert_response = timeslot_dba.insert_generated_timeslots(generated_timeslots,
+                                                                  date_today)
+        if not insert_response:
+            LOGGER.error('There was a problem generating and inserting the timeslots')
+            sys.exit()
+
     LOGGER.info("[SETUP] The timeslots were setup")
 
 
@@ -61,3 +70,11 @@ def generate_time_slots_from_range(start, end):
     LOGGER.info("[SETUP] printing the timeslots that have been generated for today")
     LOGGER.info(hours)
     return hours
+
+
+def generate_utc_date():
+    """
+    Generate the date now from UTC
+    """
+    date_today = datetime.datetime.now(pytz.utc).strftime("%m%d%Y")
+    return date_today
